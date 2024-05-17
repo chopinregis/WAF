970255a55a3970103b5119b4659283129d4626e1
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-17 19:30:23 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,16 +1,3 @@
-locals{
-  waf_policy = [for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
-  waf_list = flatten([
-    for web_firewall in local.waf_policy : [
-      for web_firewall_policy in try(app.listofwafpolicy, []) :{
-        name = web_firewall_policy.name
-
-      }
-    ]
-])
-}
-
-
 locals {
   waf_policy = [for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
   waf_list = flatten([

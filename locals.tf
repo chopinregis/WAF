71a06c67bc2bba2dc740add3c38f8173a3a4bce2
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-17 19:12:12 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals{
   window_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
   waf_list = flatten([
     for app in local.window_app : [
-      for windowapps in try(app.listofwindowapp, []) :{
+      for windowapps in try(app.listofwafpolicy, []) :{
         name = windowapps.name
         os_type = windowapps.os_type
         sku_name = windowapps.sku_name

7f62e1cf15f445cd59bd26da322d0512a07525be
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-17 20:00:34 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,3 +10,46 @@ variable "client_secret"{
 variable "tenant_id"{
   type=string
 }
+
+//
+//
+
+variable "custom_rules_name" {
+  type = string
+  default = "Rule1"
+}
+
+variable "custom_rules_priority" {
+  type = number
+  default = 1
+}
+
+variable "custom_rules_rule_type" {
+  type = string
+  default = "MatchRule"
+}
+
+variable "match_conditions_variables" {
+  type = string
+  default = "RemoteAddr"
+}
+
+variable "match_conditions_operator" {
+  type = string
+  default = "IPMatch"
+}
+
+variable "match_conditions_negation_condition" {
+  type = bool
+  default = false
+}
+
+variable "match_conditions_match_values" {
+  type = list(string)
+  default = ["192.168.1.0/24", "10.0.0.0/24"]
+}
+
+variable "custom_rules_action" {
+  type = string
+  default = "Block"
+}
